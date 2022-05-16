538fd44ce94170033ea21a2c6e41da91269a3865
isliuzinskaite <indre.sliuzinskaite@gmail.com>
2022-05-16 22:44:36 +0300

    Removed logo

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,13 +5,11 @@ import {
   Box,
 } from '@mui/material';
 import NavbarLink from './navbar-link';
-import Logo from '../logo';
 
 const Navbar: React.FC = () => (
   <AppBar position="static" sx={{ bgcolor: 'grey.900' }}>
     <Container sx={{ px: { xs: 0, sm: 0 } }}>
       <Box sx={(theme) => theme.mixins.navbar}>
-        <Logo size={40} />
         <Box sx={{ alignSelf: 'stretch' }}>
           <NavbarLink to="/">Pagrindinis</NavbarLink>
           <NavbarLink to="/vietoves">Vietovės</NavbarLink>
