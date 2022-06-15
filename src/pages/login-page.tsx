0be58655790615027de7c1faa82c83ee7166e984
isliuzinskaite <indre.sliuzinskaite@gmail.com>
2022-06-15 18:56:28 +0300

    login function

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Button,
@@ -7,73 +7,101 @@ import {
   Typography,
 } from '@mui/material';
 
-const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-  event.preventDefault();
-  const data = new FormData(event.currentTarget);
-  const email = data.get('email');
-  const password = data.get('password');
-  try {
-    const response = await fetch('http://localhost:8000/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-    });
-    // const responseJSON = await response.json();
-    if (response.status === 200) {
-      // console.log('Prisijungimas pavyko');
-    } else {
-      // console.log('Prisijungimas nepavyko');
-    }
-  } catch (error) {
-    // console.log('Prisijungimas nepavyko', error);
-  }
-};
-
 // https://github.com/mui/material-ui/blob/v5.8.4/docs/data/material/getting-started/templates/sign-in/SignIn.tsx
-const LoginPage: React.FC = () => (
-  <Container sx={{ mt: 4 }}>
-    <Box
-      sx={{
-        marginTop: 8,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-      <Typography component="h1" variant="h5">
-        Administratorių prisijungimas
-      </Typography>
-      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="El. paštas"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Slaptažodis"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Prisijungti
-        </Button>
+const LoginPage: React.FC = () => {
+  const [error, setError] = useState<any | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  // https://javascript.plainenglish.io/basic-react-login-using-external-api-e33322e480cd
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setError(null);
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email');
+    const password = data.get('password');
+    const postBody = JSON.stringify({ email, password });
+    try {
+      setLoading(true);
+      const response = await fetch('http://localhost:1337/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: postBody,
+      });
+      const responseJSON = await response.json();
+      if (response.status === 200) {
+        const adminJSON = JSON.stringify(responseJSON.admin);
+        const token = responseJSON.token.split(' ')[1];
+        localStorage.setItem('admin', adminJSON);
+        localStorage.setItem('token', token);
+        window.location.href = '/';
+      } else {
+        setError(`Prisijungti nepavyko, bandykite dar kartą. Klaida: ${responseJSON.error}`);
+      }
+    } catch (err) {
+      setError('Prisijungti nepavyko, serverio klaida.');
+    }
+    setLoading(false);
+  };
+
+  return (
+    <Container sx={{ mt: 4 }}>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Administratorių prisijungimas
+        </Typography>
+        {!loading
+          && (
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="El. paštas"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Slaptažodis"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Prisijungti
+            </Button>
+          </Box>
+          )}
+        {loading
+          && (
+          <Typography component="h1" variant="h5">Jungiamasi...</Typography>
+          )}
+        {error
+          && (
+            <Typography component="h1" variant="h5">{error}</Typography>
+          )}
       </Box>
-    </Box>
-  </Container>
-);
+    </Container>
+  );
+};
 
 export default LoginPage;
--- src/partials/navbar/index.tsx
+++ src/partials/navbar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   AppBar,
   Box,
@@ -9,30 +9,56 @@ import {
 } from '@mui/material';
 import NavbarLink from './navbar-link';
 
-const Navbar: React.FC = () => (
-  <AppBar position="static" sx={{ bgcolor: 'grey.900' }}>
-    <Toolbar sx={{ flexWrap: 'wrap' }}>
-      <Typography
-        variant="h6"
-        noWrap
-        sx={{ flexGrow: 1 }}
-      >
-        <Link href="/" underline="none">
-          Nakvok
-        </Link>
-      </Typography>
-      <Box sx={(theme) => theme.mixins.navbar}>
-        <Box sx={{ alignSelf: 'stretch' }}>
-          <NavbarLink to="/">Pagrindinis</NavbarLink>
-          <NavbarLink to="/vietoves">Vietovės</NavbarLink>
-          <NavbarLink to="/ispudziai">Įspūdžiai</NavbarLink>
+const Navbar: React.FC = () => {
+  const [admin] = useState(() => {
+    const adminString = localStorage.getItem('admin');
+    if (adminString) {
+      return JSON.parse(adminString);
+    }
+    return null;
+  });
+
+  // https://serverless-stack.com/chapters/load-the-state-from-the-session.html
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+
+  return (
+    <AppBar position="static" sx={{ bgcolor: 'grey.900' }}>
+      <Toolbar sx={{ flexWrap: 'wrap' }}>
+        <Typography
+          variant="h6"
+          noWrap
+          sx={{ flexGrow: 1 }}
+        >
+          <Link href="/" underline="none">
+            Nakvok
+          </Link>
+        </Typography>
+        <Box sx={(theme) => theme.mixins.navbar}>
+          <Box sx={{ alignSelf: 'stretch' }}>
+            <NavbarLink to="/">Pagrindinis</NavbarLink>
+            <NavbarLink to="/vietoves">Vietovės</NavbarLink>
+            <NavbarLink to="/ispudziai">Įspūdžiai</NavbarLink>
+          </Box>
         </Box>
-      </Box>
-      <Button href="/login" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
-        Prisijungti
-      </Button>
-    </Toolbar>
-  </AppBar>
-);
+        {admin
+          && (
+            <>
+              <Typography>{admin.name}</Typography>
+              <Button href="#" onClick={logout} sx={{ ml: 2 }}>Atsijungti</Button>
+            </>
+          )}
+        {!admin
+          && (
+          <Button href="/login" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+            Prisijungti
+          </Button>
+          )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
